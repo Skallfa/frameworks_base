95bd2dc34eb005262e628591c4ec6631088acdd6
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-05-09 18:18:59 +0530

    Update ScreenRecordDialog.java

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
@@ -66,6 +66,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private static final List<ScreenRecordingAudioSource> MODES = Arrays.asList(INTERNAL, MIC,
             MIC_AND_INTERNAL);
     private static final long DELAY_MS = 3000;
+    private static final long NO_DELAY = 100; 	
     private static final long INTERVAL_MS = 1000;
     private static final String PREFS = "screenrecord_";
     private static final String PREF_TAPS = "show_taps";
@@ -75,6 +76,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private static final String PREF_HEVC = "use_hevc";
     private static final String PREF_AUDIO = "use_audio";
     private static final String PREF_AUDIO_SOURCE = "audio_source";
+    private static final String PREF_SKIP = "skip_timer";
 
     private final RecordingController mController;
     private final UserContextProvider mUserContextProvider;
@@ -90,6 +92,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private Switch mHEVCSwitch;
     private Switch mAudioSwitch;
     private Spinner mOptions;
+    private Switch mSkipSwitch;	
 
     public ScreenRecordDialog(Context context, RecordingController controller,
             ActivityStarter activityStarter, UserContextProvider userContextProvider,
@@ -161,6 +164,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
         mAudioSwitch = findViewById(R.id.screenrecord_audio_switch);
         mTapsSwitch = findViewById(R.id.screenrecord_taps_switch);
         mStopDotSwitch = findViewById(R.id.screenrecord_stopdot_switch);
+	mSkipSwitch = findViewById(R.id.screenrecord_skip_switch);    
         mLowQualitySwitch = findViewById(R.id.screenrecord_lowquality_switch);
         mLongerSwitch = findViewById(R.id.screenrecord_longer_timeout_switch);
         mHEVCSwitch = findViewById(R.id.screenrecord_hevc_switch);
@@ -189,6 +193,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
         boolean lowQuality = mLowQualitySwitch.isChecked();
         boolean longerDuration = mLongerSwitch.isChecked();
         boolean hevc = mHEVCSwitch.isChecked();
+	boolean skipTime = mSkipSwitch.isChecked();    
         ScreenRecordingAudioSource audioMode = mAudioSwitch.isChecked()
                 ? (ScreenRecordingAudioSource) mOptions.getSelectedItem()
                 : NONE;
@@ -203,7 +208,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
                 RecordingService.REQUEST_CODE,
                 RecordingService.getStopIntent(userContext),
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        mController.startCountdown(DELAY_MS, INTERVAL_MS, startIntent, stopIntent);
+        mController.startCountdown(skipTime ? NO_DELAY : DELAY_MS, INTERVAL_MS, startIntent, stopIntent);
     }
 
     private class CaptureTargetResultReceiver extends ResultReceiver {
@@ -233,6 +238,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
         Prefs.putInt(userContext, PREFS + PREF_HEVC, mHEVCSwitch.isChecked() ? 1 : 0);
         Prefs.putInt(userContext, PREFS + PREF_AUDIO, mAudioSwitch.isChecked() ? 1 : 0);
         Prefs.putInt(userContext, PREFS + PREF_AUDIO_SOURCE, mOptions.getSelectedItemPosition());
+        Prefs.putInt(userContext, PREFS + PREF_SKIP, mSkipSwitch.isChecked() ? 1 : 0);		
     }
 
     private void loadPrefs() {
@@ -244,5 +250,6 @@ public class ScreenRecordDialog extends SystemUIDialog {
         mHEVCSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_HEVC, 1) == 1);
         mAudioSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_AUDIO, 0) == 1);
         mOptions.setSelection(Prefs.getInt(userContext, PREFS + PREF_AUDIO_SOURCE, 0));
+	mSkipSwitch.setChecked(Prefs.getInt(mUserContext, PREFS + PREF_SKIP, 0) == 1);    
     }
 }

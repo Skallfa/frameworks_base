8096a1338717effd504a78d48768b814bf1c51ab
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-05-11 13:39:16 +0530

    Update KeyguardIndicationController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -190,7 +190,7 @@ public class KeyguardIndicationController {
     private int mChargingCurrent;
     private double mChargingVoltage;
     private float mTemperature;
-    private int mBatteryCurrentDivider;
+    private int mBatteryChargingCurrentThreshold;
     private String mBiometricErrorMessageToShowOnScreenOn;
     private final Set<Integer> mCoExFaceAcquisitionMsgIdsToShow;
     private final FaceHelpMessageDeferral mFaceAcquiredMessageDeferral;
@@ -274,7 +274,6 @@ public class KeyguardIndicationController {
         mScreenLifecycle = screenLifecycle;
         mKeyguardLogger = keyguardLogger;
         mScreenLifecycle.addObserver(mScreenObserver);
-        mBatteryCurrentDivider = 1000; 
 
         mFaceAcquiredMessageDeferral = faceHelpMessageDeferral;
         mCoExFaceAcquisitionMsgIdsToShow = new HashSet<>();
@@ -973,7 +972,7 @@ public class KeyguardIndicationController {
             Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT) == 1;
          if (showbatteryInfo) {
             if (mChargingCurrent > 0) {
-                current = (mChargingCurrent / mBatteryCurrentDivider);
+                current = mChargingCurrent >= 23000 ? mChargingCurrent / 1000 : mChargingCurrent;
                 batteryInfo = batteryInfo + current + "mA";
             }
             if (mChargingVoltage > 0 && mChargingCurrent > 0) {

f853040ed5184dab78a864478f87656864724fc8
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-05-13 18:04:00 +0530

    Update KeyguardIndicationController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -968,6 +968,8 @@ public class KeyguardIndicationController {
         }
         
         String batteryInfo = "";
+        int current = 0;
+        double voltage = 0;
         boolean showbatteryInfo = Settings.System.getIntForUser(mContext.getContentResolver(),
             Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT) == 1;
          if (showbatteryInfo) {

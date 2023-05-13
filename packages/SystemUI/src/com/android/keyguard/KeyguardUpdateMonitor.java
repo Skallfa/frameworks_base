17761b2014f958f2723df880675bd743995fd1e3
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-05-13 19:51:51 +0530

    Update KeyguardUpdateMonitor.java

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2255,7 +2255,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, true, 0, 0, 0, false, false, false, false);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, false, true, 0, 0, 0);
         
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();

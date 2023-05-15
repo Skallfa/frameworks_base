659f90bcf35439881c103be47daae9e24b2e0371
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-05-15 11:09:16 +0530

    Update AnimatorManager.cpp

--- libs/hwui/AnimatorManager.cpp
+++ libs/hwui/AnimatorManager.cpp
@@ -74,14 +74,24 @@ void AnimatorManager::pushStaging() {
                   mParent.getName());
             return;
         }
+        
+        // Prevent multiple threads from accessing the list
+        std::mutex mNewAnimatorsMutex;
+
+        // Acquire the lock 
+        std::lock_guard<std::mutex> lock(mNewAnimatorsMutex);
 
         // Only add new animators that are not already in the mAnimators list
-        for (auto& anim : mNewAnimators) {
-            if (anim->target() != &mParent) {
-                mAnimators.push_back(std::move(anim));
+        if (mNewAnimators.size()) {
+            for (auto& anim : mNewAnimators) {
+            	// Safety check for null pointer
+                if (anim != nullptr && anim->target() != &mParent) {
+                    mAnimators.push_back(std::move(anim));
+                }
             }
+            // Clear the list.
+            mNewAnimators.clear();
         }
-        mNewAnimators.clear();
     }
 
     if (mCancelAllAnimators) {

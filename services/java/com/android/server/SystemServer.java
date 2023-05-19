a8a4355966e89354587f93fc9b2b1b23fdf55720
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-05-19 18:53:46 +0530

    Update SystemServer.java

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -3104,6 +3104,8 @@ public final class SystemServer implements Dumpable {
                 reportWtf("Triggering OdsignStatsLogger", e);
             }
             t.traceEnd();
+		
+	    com.android.server.ext.SystemServerExt.init(mSystemContext, mPackageManagerService);
         }, t);
 
         t.traceBegin("StartSystemUI");
